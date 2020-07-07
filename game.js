8db66d7687086eac8a7cb146ed99ddf36807fa96
Alireza B <Alireza@simor.org>
2020-07-07 15:04:02 -0600

    completed

--- game.js
+++ game.js
@@ -1,63 +1,35 @@
-var buttonColors =["red", "blue", "green", "yellow"];
-var gamePattern  = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-var userClickedPattern =[];
-var level = 0;
+var gamePattern = [];
+var userClickedPattern = [];
 
 var started = false;
+var level = 0;
 
-function newSequence(){
-
-  level++ ;
-  $("#level-title").text("Level " + level);
-  var randomNumber = Math.floor(Math.random()*4);
-  randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
-
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColor);
-
-
-}
-
-$(".btn").click(function(){
-  var userChosenColor = $(this).attr("id");
-  userClickedPattern.push(userChosenColor);
-  playSound(userChosenColor)
-
-});
-
-function playSound(name) {
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
-
-}
-
-function animatePress(currentColor){
-  $("#" + currentColor).addClass("pressed");
-
-  //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
-}
-
-$(".btn").keydown(function(){
-  nextSequence()
-}
-
-$(document).keypress(function() {
+$(document).keydown(function() {
   if (!started) {
-
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      console.log("success");
 
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
@@ -69,6 +41,50 @@ function checkAnswer(currentLevel) {
 
       console.log("wrong");
 
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      //2. Call startOver() if the user gets the sequence wrong.
+      startOver();
     }
+
+}
+
+function nextSequence() {
+
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function startOver(){
+  level = 0 ;
+  gamePattern = [];
+  started = false;
 
 }
